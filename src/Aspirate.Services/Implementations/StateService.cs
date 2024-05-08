4cd46b231ad4d97d346c96ce2a623285153009ba
Dave Sekula <dave.sekula@gmail.com>
2024-05-08 13:47:23 +0100

    Save the state file still, as thats where secrets now live. Just dont load all state when --disable-state is true.
    If no secrets - dont save state if its disabled

--- src/Aspirate.Services/Implementations/StateService.cs
+++ src/Aspirate.Services/Implementations/StateService.cs
@@ -15,7 +15,7 @@ public class StateService(IFileSystem fs, IAnsiConsole logger, ISecretProvider s
 
     public async Task SaveState(StateManagementOptions options)
     {
-        if (options.DisableState == true)
+        if (options.DisableState == true && options.State.SecretState?.Secrets.Any() == false)
         {
             return;
         }
