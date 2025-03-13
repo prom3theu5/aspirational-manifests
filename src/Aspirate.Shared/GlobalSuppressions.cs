944fd49821e93eef05fc40b7ca1b867454a32443
Marius Kvamme <Marius.Kvamme@no.experis.com>
2025-03-13 15:27:03 +0100

    Removed global suppression files that were just supposed to be there locally on my machine

--- src/Aspirate.Shared/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-
-[assembly: SuppressMessage("Style", "IDE0305:Simplify collection initialization", Justification = "<Pending>", Scope = "member", Target = "~M:Aspirate.Shared.Extensions.ResourceExtensions.MapComposeVolumes(System.Collections.Generic.KeyValuePair{System.String,Aspirate.Shared.Models.AspireManifests.Resource},System.String)~System.String[]")]
